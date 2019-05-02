cb14fce2b12c1cf4942c7206ada372015ac122bf
Allen Benzon <adolfoallen.benzon@nowcom.com.ph>
2019-05-02 14:07:37 +0800

    user resp model

---
+++ AdaptiveHR/Model/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdaptiveHR.Model
+{
+    public class UserInfo
+    {
+        public string token { get; set; }
+        public string email { get; set; }
+        public string name { get; set; }
+    }
+}
