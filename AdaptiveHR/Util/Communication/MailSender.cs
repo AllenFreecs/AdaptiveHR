3e376fc9307ec842be4585f51c66b52d865e4f11
Allen Benzon <adolfoallen.benzon@nowcom.com.ph>
2019-05-02 17:24:21 +0800

    send email

---
+++ AdaptiveHR/Util/Communication/MailSender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace AdaptiveHR.Util.Communication
+{
+    public class MailSender
+    {
+        public MailSender()
+        {
+
+        }
+        public void Send(string email)
+        {
+            try
+            {
+                var client = new SmtpClient("smtp.gmail.com", 587)
+                {
+                    Credentials = new NetworkCredential("myusername@gmail.com", "mypwd"),
+                    EnableSsl = true
+                };
+                client.Send("myusername@gmail.com", "myusername@gmail.com", "test", "testbody");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
